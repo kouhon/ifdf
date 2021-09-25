70004b1a996f49b982bf0eb4654d44944b78dac4
kouhon <kouichi812@gmail.com>
2021-09-26 06:22:13 +0900

    code-2-6-13

--- lib/shrinkable_footer_page.dart
+++ lib/shrinkable_footer_page.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
 class ShrinkableFooterPage extends StatefulWidget {
@@ -12,62 +12,105 @@ class ShrinkableFooterPage extends StatefulWidget {
 }
 
 class _ShrinkableFooterPageState extends State<ShrinkableFooterPage> {
+  final _scrollController = ScrollController();
+  bool isHiding = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _listenScroll();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(() {});
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          Container(
-            padding: const EdgeInsets.only(left: 16.0, right: 50),
-            height: 200,
-            width: double.infinity,
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                bottomRight: Radius.circular(20.0),
-                bottomLeft: Radius.circular(20.0)
-              ),
-              color: Color(0xFFFEEAE6)
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: const [
-                SizedBox(height: 70),
-                Text(
-                  'スクロールに応じて\nBottomNavigationBarが\n縮みます。',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 24.0,
-                    color: Color(0xFF442C2E),
-                    height: 1.1
+      backgroundColor: Colors.grey.shade100,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            ListView(
+              controller: _scrollController,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(left: 16.0, right: 50),
+                  height: 200,
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(20.0),
+                      bottomLeft: Radius.circular(20.0)
+                    ),
+                    color: Color(0xFFFEEAE6)
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: const [
+                      SizedBox(height: 70),
+                      Text(
+                        'スクロールに応じて\nBottomNavigationBarが\n縮みます。',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24.0,
+                          color: Color(0xFF442C2E),
+                          height: 1.1
+                        ),
+                      ),
+                      SizedBox(height: 20.0)
+                    ],
                   ),
                 ),
-                SizedBox(height: 20.0)
+                const SizedBox(height: 20.0),
+                Image.network('https://images.unsplash.com/photo-1528164344705-47542687000d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1369&q=80'),
+                const SizedBox(height: 20),
+                Image.network('https://images.unsplash.com/photo-1528164344705-47542687000d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1369&q=80'),
+                const SizedBox(height: 20),
+                Image.network('https://images.unsplash.com/photo-1528164344705-47542687000d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1369&q=80'),
+                const SizedBox(height: 20),
+                Image.network('https://images.unsplash.com/photo-1528164344705-47542687000d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1369&q=80'),
+                const SizedBox(height: 20),
+                Image.network('https://images.unsplash.com/photo-1528164344705-47542687000d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1369&q=80'),
+                const SizedBox(height: 20)
               ],
             ),
-          ),
-          const SizedBox(height: 20.0),
-          Image.network('https://images.unsplash.com/photo-1528164344705-47542687000d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1369&q=80'),
-          const SizedBox(height: 20),
-          Image.network('https://images.unsplash.com/photo-1528164344705-47542687000d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1369&q=80'),
-          const SizedBox(height: 20),
-          Image.network('https://images.unsplash.com/photo-1528164344705-47542687000d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1369&q=80'),
-          const SizedBox(height: 20),
-          Image.network('https://images.unsplash.com/photo-1528164344705-47542687000d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1369&q=80'),
-          const SizedBox(height: 20),
-          Image.network('https://images.unsplash.com/photo-1528164344705-47542687000d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1369&q=80'),
-          const SizedBox(height: 20)
-        ],
+            Positioned(
+              bottom: 0,
+              child: _BottomNavigationBar(isHiding: isHiding),
+            )
+          ]
+        ),
       )
     );
+  }
+
+  void _listenScroll() {
+    _scrollController.addListener(() {
+      if (_scrollController.position.userScrollDirection == ScrollDirection.reverse) {
+        isHiding = true;
+      } else {
+        isHiding = false;
+      }
+      setState(() {});
+    });
   }
 }
 
 class _BottomNavigationBar extends StatelessWidget {
+  const _BottomNavigationBar({required this.isHiding});
+  
+  final bool isHiding;
+  
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
-    return Container(
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 200),
       height: 60,
       color: const Color(0xfffeeae6),
       child: Padding(
