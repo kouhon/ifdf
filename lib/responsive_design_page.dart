5e25fe072c35c2aec9e5264ceeaed4da5468d8b4
kouhon <kouichi812@gmail.com>
2021-09-26 17:49:04 +0900

    code-2-7-7

--- lib/responsive_design_page.dart
+++ lib/responsive_design_page.dart
@@ -24,78 +24,83 @@ class _Body extends StatelessWidget {
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
     return Center(
-      child: Column(
-        children: [
-          const Spacer(),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 50),
-            child: Text(
-              "Flutter is Google's UI toolkit for building beautiful, natively compiled",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-                height: 1.85,
-                fontWeight: FontWeight.bold
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(
+          maxWidth: 650,
+        ),
+        child: Column(
+          children: [
+            const Spacer(),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 50),
+              child: Text(
+                "Flutter is Google's UI toolkit for building beautiful, natively compiled",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  height: 1.85,
+                  fontWeight: FontWeight.bold
+                ),
               ),
             ),
-          ),
-          const Spacer(),
-          FlutterLogo(
-            size: height * 0.3,
-          ),
-          const Spacer(),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 50),
-            child: Text(
-              'Fast Development Paint your app to life in milliseconds with Stateful Hot Reload. Use a rich set of ',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 15,
-                height: 1.85
+            const Spacer(),
+            FlutterLogo(
+              size: height * 0.3,
+            ),
+            const Spacer(),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 50),
+              child: Text(
+                'Fast Development Paint your app to life in milliseconds with Stateful Hot Reload. Use a rich set of ',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                  height: 1.85
+                ),
               ),
             ),
-          ),
-          const Spacer(),
-          ConstrainedBox(
-            constraints: const BoxConstraints(
-              maxWidth: 650
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 50),
-              child: SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      textStyle: const TextStyle(
-                          color: Colors.white
-                      ),
-                      primary: Theme.of(context).colorScheme.secondary,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(4)
-                      )
-                  ),
-                  onPressed: () {},
-                  child: const Padding(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: 15
+            const Spacer(),
+            ConstrainedBox(
+              constraints: const BoxConstraints(
+                maxWidth: 650
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 50),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        textStyle: const TextStyle(
+                            color: Colors.white
+                        ),
+                        primary: Theme.of(context).colorScheme.secondary,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(4)
+                        )
                     ),
-                    child: Text(
-                      'get started',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold
+                    onPressed: () {},
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 15
+                      ),
+                      child: Text(
+                        'get started',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
             ),
-          ),
-          const Spacer(),
-        ],
+            const Spacer(),
+          ],
+        ),
       ),
     );
   }
