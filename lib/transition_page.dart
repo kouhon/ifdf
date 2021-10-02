0c926b773001d435de44aec835a4b0da6c694f26
kouhon <kouichi812@gmail.com>
2021-10-02 14:18:55 +0900

    code-3-4-2

--- lib/transition_page.dart
+++ lib/transition_page.dart
@@ -32,6 +32,20 @@ class TransitionPage extends StatelessWidget {
                     PageRouteBuilder(
                       pageBuilder: (context, animation, secondaryAnimation) {
                         return const _PageA();
+                      },
+                      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                        return SlideTransition(
+                          position: Tween(
+                            begin: const Offset(-1, -1),
+                            end: const Offset(0, 0)
+                          ).animate(
+                            CurvedAnimation(
+                              parent: animation,
+                              curve: Curves.easeOutCubic
+                            )
+                          ),
+                          child: child,
+                        );
                       }
                     )
                   );
