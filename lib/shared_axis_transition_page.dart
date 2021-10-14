9da793528d921aaee4922da060cbc225a34782b5
kouhon <kouichi812@gmail.com>
2021-10-14 10:32:12 +0900

    code3-5-5

--- lib/shared_axis_transition_page.dart
+++ lib/shared_axis_transition_page.dart
@@ -1,5 +1,58 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+
+class SharedAxisTransitionPage extends StatefulWidget {
+  const SharedAxisTransitionPage({Key? key}) : super(key: key);
+
+  @override
+  _SharedAxisTransitionPageState createState() => _SharedAxisTransitionPageState();
+}
+
+class _SharedAxisTransitionPageState extends State<SharedAxisTransitionPage> {
+  final SharedAxisTransitionType _transitionType = SharedAxisTransitionType.horizontal;
+  bool _isCatImage = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: PageTransitionSwitcher(
+                duration: const Duration(milliseconds: 800),
+                reverse: !_isCatImage,
+                transitionBuilder: (Widget child, Animation<double> animation, Animation<double> secondaryAnimation) => SharedAxisTransition(
+                  child: child,
+                  animation: animation,
+                  secondaryAnimation: secondaryAnimation,
+                  transitionType: _transitionType,
+                ),
+                child: _isCatImage ? const _CatImage() : const _DogImage(),
+              ),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.grey[300],
+                onPrimary: Colors.black,
+              ),
+              onPressed: _toggleImages,
+              child: const Text('切り替える'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _toggleImages() {
+    setState(() {
+      _isCatImage = !_isCatImage;
+    });
+  }
+}
+
 
 class _CatImage extends StatelessWidget {
   const _CatImage({Key? key}) : super(key: key);
