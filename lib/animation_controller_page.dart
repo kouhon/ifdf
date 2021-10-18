56970fd97172bce2e67d6c86bf049b996286406c
kouhon <kouichi812@gmail.com>
2021-10-18 15:34:10 +0900

    code-3-6-1

---
+++ lib/animation_controller_page.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'dart:math' as math;
+
+class AnimationControllerPage extends StatefulWidget {
+  const AnimationControllerPage({Key? key}) : super(key: key);
+
+  @override
+  _AnimationControllerPageState createState() => _AnimationControllerPageState();
+}
+
+class _AnimationControllerPageState extends State<AnimationControllerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              '_animationController\n.value = ',
+              style: TextStyle(
+                fontSize: 22
+              ),
+            ),
+            const Text(
+              '1.00',
+              style: TextStyle(
+                fontSize: 64
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            SizedBox(
+              width: 130,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.grey[300],
+                  onPrimary: Colors.black,
+                ),
+                onPressed: () {},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Icon(Icons.play_arrow),
+                    Text(
+                      '再生'
+                    )
+                  ],
+                ),
+              )
+            ),
+            SizedBox(
+              width: 130,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.grey[300],
+                  onPrimary: Colors.black,
+                ),
+                onPressed: () {},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Transform.rotate(
+                      angle: math.pi,
+                      child: const Icon(Icons.play_arrow),
+                    ),
+                    const Text(
+                      '逆再生'
+                    )
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 130,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.grey[300],
+                  onPrimary: Colors.black
+                ),
+                onPressed: () {},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Transform.rotate(
+                      angle: math.pi,
+                      child: const Icon(Icons.pause),
+                    ),
+                    const Text(
+                      '一時停止'
+                    )
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 130,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.grey[300],
+                  onPrimary: Colors.black,
+                ),
+                onPressed: () {},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Icon(Icons.stop),
+                    Text(
+                      '停止'
+                    )
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 130,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.grey[300],
+                  onPrimary: Colors.black
+                ),
+                onPressed: () {},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Icon(Icons.repeat),
+                    Text(
+                      'リピート'
+                    )
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 22,
+            ),
+            const Text(
+              'Duration',
+              style: TextStyle(
+                fontSize: 22
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Radio(value: null, groupValue: null, onChanged: null,),
+                Text('15秒'),
+                Radio(value: null, groupValue: null, onChanged: null,),
+                Text('30秒'),
+                Radio(value: null, groupValue: null, onChanged: null,),
+                Text('60秒'),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ifdf/shared_axis_transition_page.dart';
+import 'animation_controller_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const SharedAxisTransitionPage()
+      home: const AnimationControllerPage()
     );
   }
 }
