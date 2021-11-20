42b5f15855fe1afab59277bd9b9ce06a8cabdd50
kouhon <kouichi812@gmail.com>
2021-11-21 07:45:00 +0900

    code-4-4-1

--- lib/models/todos/todo.dart
+++ lib/models/todos/todo.dart
@@ -1,6 +1,8 @@
 class Todo {
-  const Todo({this.isCompleted = false, required this.name});
+  const Todo({this.isCompleted = false, required this.name, id});
 
   final bool isCompleted;
   final String name;
+
+  static fromJson(decode) {}
 }
